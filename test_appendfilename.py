a3cd2a441e77971dbb270807554179906215c232
Norwid Behrnd <nbehrnd@yahoo.com>
2024-11-05 16:02:55 +0100

    refactor: edit tests to smartly prepend a string
    The third section of tests about smartly prepending a string -- i.e.
    to insert a string right after the time or date stamp -- was edited.
    Manual tests in Linux Debian and Windows 10 as well as with pytest
    on either operating system confirm inconsistencies of the format
    of the file name eventually generated.[1,2]
    
    In addition, the implementation differs in robustness to deploy
    special characters like for instance the underscore as a separator
    to enclose the string to add.  Instead of `_` (as in Debian), Windows
    tends to enclose these separators with single quotes (`'_'`) instead.
    The default by `appendfilename` to use a single blank character
    still is considered "safe to be used" _for `appendfilename`_ though
    e.g. the underscore would represent a safe alternative if file names
    with white space are not acceptable.
    
    [1] https://github.com/novoid/appendfilename/issues/15
    [2] https://github.com/novoid/appendfilename/issues/16
    
    Signed-off-by: Norwid Behrnd <nbehrnd@yahoo.com>

--- test_appendfilename.py
+++ test_appendfilename.py
@@ -4,7 +4,7 @@
 # author:  nbehrnd@yahoo.com
 # license: GPL v3, 2022.
 # date:    2022-01-05 (YYYY-MM-DD)
-# edit:    [2024-11-03 Sun]
+# edit:    [2024-11-05 Tue]
 #
 """Test pad for functions by appendfilename with pytest.
 
@@ -171,5 +171,129 @@ def test_prepend(arg1, arg2, arg3, arg4):
     assert os.path.isfile(new_filename)
 
     # check if the OS can process the new file / space cleaning
+    os.remove(new_filename)
+    assert os.path.isfile(new_filename) is False
+
+# This section tests the insertion of a string into the file's file name
+# just after the file's time or date stamp as provided `date2name`.
+
+arg1_values = [
+    "2021-12-31T18.48.22_test.txt",
+    "2021-12-31_test.txt",
+#    "20211231_test.txt",  # by now `20211231_test.txt` -> 20211231_test ping.txt
+#    "2021-12_test.txt",   # by now `2021-12_test.txt` -> `2021-12_test ping.txt`
+#    "211231_test.txt"     # by now `211231_test.txt` -> `211231_test ping.txt`
+]
+arg2_values = [
+    "-t book",
+    "-t book_shelf",
+    "--text book",
+    "--text book_shelf"
+]
+arg3_values = [
+    "",  # i.e. fall back to default single space
+#    "--separator '!'",
+#    "--separator '@'",
+#    "--separator '#'",
+#    "--separator '$'",
+#    "--separator '%'",
+#    "--separator '_'",
+#    "--separator '+'",
+#    "--separator '='",
+#    "--separator '-'"
+]
+# Note: The check with pytest and `*` as separator in Windows 10 fails.
+# Contrasting to Linux Debian 13, a `pytest` in Windows 10 revealed every
+# of these special characters can not safely used as an additional separator.
+
+# create the permutations:
+test_cases = list(product(arg1_values, arg2_values, arg3_values))
+
+@pytest.mark.parametrize("arg1, arg2, arg3", test_cases)
+def test_smart_prepend(arg1, arg2, arg3):
+    """test the insertion of a new string just past the time stamp
+
+    arg1   the test file to process, partly inspired by `date2name`
+    arg2   the text string to be added
+    arg3   the separator (at least in Windows 10, do not use `*`
+    """
+    time_stamp = ""
+    time_stamp_separator = ""
+    old_filename_no_timestamp = ""
+
+    # create a test file:
+    with open(arg1, mode="w", encoding="utf-8") as newfile:
+        newfile.write("this is a placeholder\n")
+
+    #run `appendfilename` on this test file
+    run_appendfilename = " ".join(
+        ["python", PROGRAM, arg1, arg2, arg3, " --smart-prepend"])
+    subprocess.run(run_appendfilename, shell=True, check = True)
+
+    # construct the new file name to be testedt:
+    old_filename = arg1
+
+    # account for the implicit separator, i.e. the single space:
+    if len(shlex.split(arg3)) == 0:
+        separator = " "
+    else:
+        separator = shlex.split(arg3)[1]
+
+    # Time stamps `date2name` provides can be either one of five formats
+    #
+    # YYYY-MM-DDTHH.MM.SS   `--withtime`
+    # YYYY-MM-DD            default
+    # YYYYMMDD              `--compact`
+    # YYYY-MM               `--month`
+    # YYMMDD                `--short`
+
+    # Currently, one observes two patterns by `appendfilename`: one which
+    # substitutes the separator by `date2name`, the other which retains it.
+    # Note patterns `compact`, `month`, and `short`, currently append the
+    # additional string rather than smartly prepend after the date stamp --
+    # for now, these three are not tested.  Equally see discussions 15 and 16,
+    # https://github.com/novoid/appendfilename/issues/15
+    # https://github.com/novoid/appendfilename/issues/16
+
+    # pattern `--with-time`
+    if re.search(r"^\d{4}-[012]\d-[0-3]\dT[012]\d\.[0-5]\d\.[0-5]\d", old_filename):
+        time_stamp = old_filename[:19]
+        time_stamp_separator = old_filename[19]
+        old_filename_no_timestamp = old_filename[20:]
+
+    # default pattern
+    elif re.search(r"^\d{4}-[012]\d-[0-3]\d", old_filename):
+        time_stamp = old_filename[:10]
+        time_stamp_separator = old_filename[10]
+        old_filename_no_timestamp = old_filename[11:]
+
+    # pattern `--compact`  # currently fails
+    elif re.search(r"^\d{4}[012]\d[0-3]\d", old_filename):
+        time_stamp = old_filename[:8]
+        time_stamp_separator = old_filename[8]
+        old_filename_no_timestamp = old_filename[9:]
+
+    # pattern `--month`  # currently fails
+    elif re.search(r"^\d{4}-[012]\d", old_filename):
+        time_stamp = old_filename[:7]
+        time_stamp_separator = old_filename[7]
+        old_filename_no_timestamp = old_filename[8:]
+
+    # pattern `--short`  # currently fails
+    elif re.search(r"^\d{4}[012]\d[012]\d", old_filename):
+        time_stamp = old_filename[:6]
+        time_stamp_separator = old_filename[6]
+        old_filename_no_timestamp = old_filename[7:]
+
+    new_filename = "".join([time_stamp, #time_stamp_separator,
+        separator, shlex.split(arg2)[1], separator,
+        old_filename_no_timestamp ])
+
+    # is the new file present?
+    print("\nnew_filename")  # optional check for `pytest -s`
+    print(new_filename)
+    assert os.path.isfile(new_filename)
+
+    # check if the IS can process the new file / space cleaning
     os.remove(new_filename)
     assert os.path.isfile(new_filename) is False
